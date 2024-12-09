6215cc2a6f7971de164cf83401a1149c47cee980
unknown <gutemaurgawa@gmail.com>
2024-12-09 14:20:31 +0300

    functional component

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import Header from './components/subComponent/Header.jsx'
+import Content from './components/subComponent/Content.jsx'
+import Footer from './components/subComponent/Footer.jsx'
+
+function App() {
+
+  return(
+   <div>
+      <Header/>
+      <Content/>
+      <Footer/>
+   </div>
+  )
+}
+export default App
---
+++ src/components/Content.jsx
@@ -0,0 +1,16 @@
+function Content()
+{
+  return (
+<main>
+  <ul>
+  <li>Apples</li>
+  <li>Bananas </li>
+  <li>Oranges </li>
+  <li>Grapes</li>
+  <li>Strawberries</li>
+  <li>Lemons</li>
+  </ul>
+</main>
+  )
+}
+export default Content;
